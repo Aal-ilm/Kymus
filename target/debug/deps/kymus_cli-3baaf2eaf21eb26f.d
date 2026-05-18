d4d9f680179278c83aea6e62245b403ed8fab8bf
Aal M <admin@dynatheia.com>
2026-05-17 22:10:25 -0400

    Remove target from tracking

--- target/debug/deps/kymus_cli-3baaf2eaf21eb26f.d
+++
@@ -1,5 +0,0 @@
-/Users/aalm/Developer/Kymus/target/debug/deps/kymus_cli-3baaf2eaf21eb26f.d: kymus-cli/src/main.rs
-
-/Users/aalm/Developer/Kymus/target/debug/deps/kymus_cli-3baaf2eaf21eb26f: kymus-cli/src/main.rs
-
-kymus-cli/src/main.rs:
--- target/debug/deps/kymus_core-2aa147f3aabac296.d
+++
@@ -1,9 +0,0 @@
-/Users/aalm/Developer/Kymus/target/debug/deps/kymus_core-2aa147f3aabac296.d: kymus-core/src/lib.rs kymus-core/src/encoder.rs kymus-core/src/decoder.rs kymus-core/src/codebook.rs kymus-core/src/../../codebooks/english-60k.txt
-
-/Users/aalm/Developer/Kymus/target/debug/deps/kymus_core-2aa147f3aabac296: kymus-core/src/lib.rs kymus-core/src/encoder.rs kymus-core/src/decoder.rs kymus-core/src/codebook.rs kymus-core/src/../../codebooks/english-60k.txt
-
-kymus-core/src/lib.rs:
-kymus-core/src/encoder.rs:
-kymus-core/src/decoder.rs:
-kymus-core/src/codebook.rs:
-kymus-core/src/../../codebooks/english-60k.txt:
